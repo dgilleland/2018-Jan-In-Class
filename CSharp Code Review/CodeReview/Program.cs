6654977d6f28e19cdd0b4726a40122e6e2c23a3e
Dan Gilleland <dgilleland@nait.ca>
2018-01-11 16:01:39 -0700

    Updated the Simple Code Review program

--- CSharp Code Review/CodeReview/Program.cs
+++ CSharp Code Review/CodeReview/Program.cs
@@ -10,11 +10,53 @@ namespace CodeReview
     {
         static void Main(string[] args)
         {
-            // Use the Salutation class for a Walmart Greeter.
-            Salutation oldGuy = new Salutation("Welcome to Walmart!", "Thanks for shopping at Walmart!");
-            // Get the guy to talk....
-            Console.WriteLine(oldGuy.Greet());
-            Console.WriteLine(oldGuy.SayFarewell());
+            var app = new Program();
+            app.Run();
+        }
+
+        // Our "Program" is really the "heart" of our "application".
+        // So, let's treat it like an object.
+        // We'll use a property that holds a bunch of Salutation objects.
+        public List<Salutation> Speakers { get; private set; }
+        public Program()
+        {
+            // The job of a constructor is to make sure that all the
+            // properties/fields of the object are set to "meaningful values"
+            Speakers = new List<Salutation>(); // an "empty" list
+            Speakers.Add(new Salutation("Welcome to Walmart!", "Thanks for shopping at Walmart!"));
+            Speakers.Add(new Salutation("nuqneH!", "Qapla'!"));
+        }
+
+        public void Run()
+        {
+            Console.ForegroundColor = ConsoleColor.DarkGreen;
+            Console.WriteLine("You have entered a large building. Two beings stand before you.");
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.Write("Who do you want to speak to? Person A or B? ");
+            string userInput = Console.ReadLine().ToUpper();
+            while(userInput != "A" && userInput != "B")
+            {
+                Console.Write("Try again:");
+                userInput = Console.ReadLine().ToUpper();
+            }
+
+            Console.ResetColor();
+            switch(userInput)
+            {
+                case "A":
+                    Speak(Speakers[0]);
+                    break;
+                case "B":
+                    Speak(Speakers[1]);
+                    break;
+            }
+        }
+
+
+        private void Speak(Salutation speaker)
+        {
+            Console.WriteLine(speaker.Greet());
+            Console.WriteLine(speaker.SayFarewell());
         }
     }
 }
--- CSharp Code Review/CodeReview/Salutation.cs
+++ CSharp Code Review/CodeReview/Salutation.cs
@@ -6,11 +6,23 @@ using System.Threading.Tasks;
 
 namespace CodeReview
 {
+    /// <summary>
+    /// The Salutation class represents something that can say hello or goodbye
+    /// </summary>
     public class Salutation
     {
+        #region Properties and Fields - What the Salutation objects will "look like"
+        // Fields
         private string _Greeting;
         private string _Farewell;
+        #endregion
 
+        #region Constructor and Methods - How the Salutation objects will "behave"
+        /// <summary>
+        /// Instantiates a Salutation object with appropriate messages
+        /// </summary>
+        /// <param name="greetingMessage">The message to use when greeting</param>
+        /// <param name="farewellMessage">The message to use to say goodbye</param>
         public Salutation(string greetingMessage, string farewellMessage)
         {
             _Greeting = greetingMessage;
@@ -26,5 +38,6 @@ namespace CodeReview
         {
             return _Farewell;
         }
+        #endregion
     }
 }
