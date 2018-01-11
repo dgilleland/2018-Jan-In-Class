68b4ee567e55b8702a5eae4ca491df1763ae1621
Dan Gilleland <dgilleland@nait.ca>
2018-01-11 15:06:32 -0700

    Where we left off last class

---
+++ CSharp Code Review/CodeReview/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeReview
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Use the Salutation class for a Walmart Greeter.
+            Salutation oldGuy = new Salutation("Welcome to Walmart!", "Thanks for shopping at Walmart!");
+            // Get the guy to talk....
+            Console.WriteLine(oldGuy.Greet());
+            Console.WriteLine(oldGuy.SayFarewell());
+        }
+    }
+}
---
+++ CSharp Code Review/CodeReview/Salutation.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeReview
+{
+    public class Salutation
+    {
+        private string _Greeting;
+        private string _Farewell;
+
+        public Salutation(string greetingMessage, string farewellMessage)
+        {
+            _Greeting = greetingMessage;
+            _Farewell = farewellMessage;
+        }
+
+        public string Greet()
+        {
+            return _Greeting;
+        }
+
+        public string SayFarewell()
+        {
+            return _Farewell;
+        }
+    }
+}
