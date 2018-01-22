cc02ead439533fb2dfd689081c09dafd3b7fd212
Dan Gilleland <dgilleland@nait.ca>
2018-01-22 09:36:44 -0700

    Setup for mini-lesson on string manipulation

--- Week 03/events-intro/js/main.js
+++ Week 03/events-intro/js/main.js
@@ -1,5 +1,6 @@
 // 1. select the target element
 var featureImage = document.querySelector('img.feature');
+var featureLink = document.querySelector('a.feature.link');
 
 // 2. create the event listener function
 function handleFeatureClick() {
@@ -10,5 +11,18 @@ function handleFeatureClick() {
     desc.classList.remove('hidden');
 }
 
+function handleFeatureLinkClick(evt) {
+    console.log('a.feature.link was clicked...');
+    // set the image src to the anchor's href value
+    featureImage.src = featureLink.href;
+
+    // make the image visible
+    featureImage.classList.remove('hidden');
+
+    // do not load the image in the page
+    evt.preventDefault();
+}
+
 // 3. add event listener
 featureImage.addEventListener('click', handleFeatureClick);
+featureLink.addEventListener('click', handleFeatureLinkClick);
---
+++ Week 03/string-manipulations/js/main.js
@@ -0,0 +1,16 @@
+var urlTemplate = 'http://placehold.it/:size/:color/:background/';
+var urlQueryStringTemplate = "?text="
+
+// mini-lesson on string manipulation:
+
+//  split string into an array
+//  join from array into a string
+//  includes / endswith / startsWith
+//  match - regular expressions
+//  indexof / lastIndexOf
+//  replace
+//  character access
+//  comparing - equals, greater/lesser
+//  slice
+//  toString
+//  trim
