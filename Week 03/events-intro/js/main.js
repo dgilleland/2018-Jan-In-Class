5296d0b97a51b9e9782087c0541ed9b037c66a75
Dan Gilleland <dgilleland@nait.ca>
2018-01-22 08:24:57 -0700

    Event listener for a click on the image

--- Week 03/events-intro/js/main.js
+++ Week 03/events-intro/js/main.js
@@ -1,5 +1,14 @@
 // 1. select the target element
+var featureImage = document.querySelector('img.feature');
 
 // 2. create the event listener function
+function handleFeatureClick() {
+    console.log('img.feature was clicked...');
+    // Grab the HTMLElement with a .feature and a .description class
+    var desc = document.querySelector('.feature.description');
+    // All HTMLElement objects have .classList property that is a collection of class names
+    desc.classList.remove('hidden');
+}
 
 // 3. add event listener
+featureImage.addEventListener('click', handleFeatureClick);
