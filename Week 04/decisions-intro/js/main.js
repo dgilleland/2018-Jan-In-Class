bf76921d91ee2dcaf203d5f2823abf337f921c71
Dan Gilleland <dgilleland@nait.ca>
2018-01-29 09:18:26 -0700

    Alternate way of assigning the event listener

--- Week 04/decisions-intro/js/main.js
+++ Week 04/decisions-intro/js/main.js
@@ -1,25 +1,14 @@
-// 1. select the target element
-var featureImage = document.querySelector('img.feature');
+// An anonymous function is a function that does not have a name
+// Anonymous functions are often used when you want to create a "one-of"
+// function that is being passed into another method/function.
+document.querySelector('img.feature')
+		.addEventListener('click', function () {
+			var desc = document.querySelector('p.feature.description');
+		
+			if (desc.classList.contains('hidden')) {
+				desc.classList.remove('hidden');
+			} else {
+				desc.classList.add('hidden');
+			}
+		});
 
-// 2. create the event listener function
-function handleFeatureClick() {
-	var desc = document.querySelector('p.feature.description');
-
-	// remove the hidden class IF it is present,
-	// otherwise, add it back!
-	if (desc.classList.contains('hidden')) {
-		desc.classList.remove('hidden');
-	} else {
-		desc.classList.add('hidden');
-	}
-	
-	// achieve the same result using using string functions
-	// if (desc.className.indexOf('hidden') >= 0) {
-	// 	desc.classList.remove('hidden');
-	// } else {
-	// 	desc.classList.add('hidden');
-	// }
-}
-
-// 3. add event listener
-featureImage.addEventListener('click', handleFeatureClick);
