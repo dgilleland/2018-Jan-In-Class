ba09b1480e894b3ecb787dc8d2fe87057e1e998f
Dan Gilleland <dgilleland@nait.ca>
2018-01-29 09:19:04 -0700

    Thumbnail Resize - v0

---
+++ Week 04/thumbnail-resize/js/main.js
@@ -0,0 +1,6 @@
+// When a gallery thumbnail size button is clicked, the corresponding 
+// size class should be added to the div.thumbnails element.
+// This will apply the corresponding CSS rule to modify the display of
+// the thumbnail images.
+// In addition to this, the clicked button should be made active
+// (i.e. add the active class).
