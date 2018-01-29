317f9e34dc94d96a7bcd753bac65d87bf054ab95
Dan Gilleland <dgilleland@nait.ca>
2018-01-29 08:46:42 -0700

    Demo if statement

---
+++ Week 04/decisions-intro/js/main.js
@@ -0,0 +1,25 @@
+// 1. select the target element
+var featureImage = document.querySelector('img.feature');
+
+// 2. create the event listener function
+function handleFeatureClick() {
+	var desc = document.querySelector('p.feature.description');
+
+	// remove the hidden class IF it is present,
+	// otherwise, add it back!
+	if (desc.classList.contains('hidden')) {
+		desc.classList.remove('hidden');
+	} else {
+		desc.classList.add('hidden');
+	}
+	
+	// achieve the same result using using string functions
+	// if (desc.className.indexOf('hidden') >= 0) {
+	// 	desc.classList.remove('hidden');
+	// } else {
+	// 	desc.classList.add('hidden');
+	// }
+}
+
+// 3. add event listener
+featureImage.addEventListener('click', handleFeatureClick);
