bdfa99d16a52ca1758cbd1e28bdf7054df97de8b
Dan Gilleland <dgilleland@nait.ca>
2018-01-31 09:26:24 -0700

    Very quick demo/notes on Notifications API

---
+++ Bonus Topics/Notifications/ReadMe.md
@@ -0,0 +1,3 @@
+# The Notification API
+
+Some modern browsers support the [**Notifications API**](https://developer.mozilla.org/en-US/docs/Web/API/notification) which enable web applications to send "push" notifications to a client's computer. For more in-depth information, read the ["Using the Notifications API"](https://developer.mozilla.org/en-US/docs/Web/API/Notifications_API/Using_the_Notifications_API) article on MDN.
---
+++ Bonus Topics/Notifications/js/main.js
@@ -0,0 +1,31 @@
+document.getElementById('requestNotification').addEventListener('click', notifyMe);
+
+var clickCount = 0;
+function notifyMe() {
+    // Let's check if the browser supports notifications
+    if (!("Notification" in window)) {
+      alert("This browser does not support desktop notification");
+    }
+  
+    // Let's check whether notification permissions have already been granted
+    else if (Notification.permission === "granted") {
+      // If it's okay let's create a notification
+      clickCount++;
+      var notification = new Notification("Hi there! This is notification # " + clickCount);
+
+    }
+  
+    // Otherwise, we need to ask the user for permission
+    else if (Notification.permission !== "denied") {
+      Notification.requestPermission().then(function (permission) {
+        // If the user accepts, let's create a notification
+        if (permission === "granted") {
+          var notification = new Notification("I will let you know how many times you click the button. This is your first click.");
+          clickCount++;
+        }
+      });
+    }
+  
+    // At last, if the user has denied notifications, and you 
+    // want to be respectful there is no need to bother them any more.
+  }
