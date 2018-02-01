01c4b1eb39a626ba78fd547de98dfc1b322b5051
Dan Gilleland <dgilleland@nait.ca>
2018-02-01 11:53:50 -0700

    Create/Drop Tables

---
+++ B-DDL/ESP-DDL.sql
@@ -0,0 +1,55 @@
+/* **********************************************
+ * Simple Table Creation - Columns and Primary Keys
+ *
+ * Emergency Service & Product
+ * Specification Document 1
+ * Version 1.0.0
+ *
+ * Author: Dan Gilleland
+ ********************************************** */
+
+-- Select the CREATE DATABASE stement below to create the demo database.
+-- CREATE DATABASE [ESP-A01]
+
+USE [ESP-A01] -- this is a statement that tells us to switch to a particular database
+-- Notice in the database name above, it is "wrapped" in square brackets because 
+-- the name had a hypen in it. 
+-- For all our other objects (tables, columns, etc), we won't use hypens or spaces, so
+-- the use of square brackets are optional.
+GO  -- this statement helps to "separate" various DDL statements in our script
+    -- so that they are executed as "blocks" of code.
+
+/* DROP TABLE statements (to "clean up" the database for re-creation)  */
+IF EXISTS (SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = 'Orders')
+    DROP TABLE Orders
+IF EXISTS (SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = 'Customers')
+    DROP TABLE Customers
+
+
+
+-- To create a database table, we use the CREATE TABLE statement.
+-- Note that the order in which we create/drop tables is important
+-- because of how the tables are related via Foreign Keys.
+CREATE TABLE Customers
+(
+    -- The body of a CREATE TABLE will identify a comma-separated list of
+    -- Column Declarations and Table Constraints.
+    CustomerNumber  int,
+    FirstName       varchar(50),
+    LastName        varchar(60),
+    [Address]       varchar(40),
+    City            varchar(35),
+    Province        char(2),
+    PostalCode      char(6),
+    PhoneNumber     char(13)
+)
+
+CREATE TABLE Orders
+(
+    OrderNumber     int,
+    CustomerNumber  int,
+    [Date]          datetime,
+    Subtotal        money,
+    GST             money,
+    Total           money
+)
