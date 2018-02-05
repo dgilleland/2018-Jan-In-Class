e87f17a361bbd103dcda4a2c9285a84f8cb6250d
Dan Gilleland <dgilleland@nait.ca>
2018-02-05 09:20:18 -0700

    Notes on the Forms-Intro example

--- Week 05/forms-intro/js/main.js
+++ Week 05/forms-intro/js/main.js
@@ -1,6 +1,23 @@
 document.querySelector('form.login').addEventListener('submit', function (evt) {
-	var user = evt.target.elements.username;
-	var pass = evt.target.elements.password;
+	// The evt.target is the <form> element
+	var formElement = evt.target;
+	// A <form> HTML Element has a collection called elements
+	// which hold values that will be submitted from the form.
+	// Each <input> or other form element on the form
+	// will have the control's id attribute as a "property"
+	// (or, an index) on the elements collection.
+	var user = formElement.elements.usernameId;
+	var pass = formElement.elements.passwordInfo;
+	
+	// Any property can be accessed as though it were a 
+	// string-like "index" to an array.
+	var pass2 = formElement.elements["passwordInfo"];
+	// pass2 is an <input type="password"> element in the
+	// elements collection.
+	// pass2 has a property called "name", and a property
+	// called "value". From these, the form can construct
+	// a name-value pair to submit to the web server.
+	
 	var valid = true;
 
 	// check the username, should be email
