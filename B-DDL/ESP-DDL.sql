4aaa2ee31302277aa2dcb88fc125a8f9bd73d0cf
Dan Gilleland <dgilleland@nait.ca>
2018-02-05 11:00:01 -0700

    Constraints + Insert Data
    - NULL/NOT NULL
    - PRIMARY KEY

--- B-DDL/ESP-DDL.sql
+++ B-DDL/ESP-DDL.sql
@@ -34,22 +34,44 @@ CREATE TABLE Customers
 (
     -- The body of a CREATE TABLE will identify a comma-separated list of
     -- Column Declarations and Table Constraints.
-    CustomerNumber  int,
-    FirstName       varchar(50),
-    LastName        varchar(60),
-    [Address]       varchar(40),
-    City            varchar(35),
-    Province        char(2),
-    PostalCode      char(6),
-    PhoneNumber     char(13)
+    CustomerNumber  int
+        -- The following is a PRIMARY KEY constraint that has a specific name
+        -- Primary Key constraints ensure a row of data being added to the table
+        -- will have to have a unique value for the Primary Key column(s)
+        CONSTRAINT PK_Customers_CustomerNumber
+            PRIMARY KEY
+        -- IDENTITY means the database will generate a unique whole-number
+        -- value for this column
+        IDENTITY(100, 1) -- The first number is the "seed",
+                         -- and the last number is the "increment"
+                                        NOT NULL, -- NOT NULL means the data is required
+    FirstName       varchar(50)         NOT NULL,
+    LastName        varchar(60)         NOT NULL,
+    [Address]       varchar(40)         NOT NULL,
+    City            varchar(35)         NOT NULL,
+    Province        char(2)             NOT NULL,
+    PostalCode      char(6)             NOT NULL,
+    PhoneNumber     char(13)                NULL  -- NULL means the data is optional
 )
 
 CREATE TABLE Orders
 (
-    OrderNumber     int,
-    CustomerNumber  int,
-    [Date]          datetime,
-    Subtotal        money,
-    GST             money,
-    Total           money
+    OrderNumber     int
+        CONSTRAINT PK_Orders_OrderNumber
+            PRIMARY KEY
+        IDENTITY(200, 1)                NOT NULL,
+    CustomerNumber  int                 NOT NULL,
+    [Date]          datetime            NOT NULL,
+    Subtotal        money               NOT NULL,
+    GST             money               NOT NULL,
+    Total           money               NOT NULL
 )
+
+GO -- End of a batch of instructions
+
+-- Let's insert a few rows of data for the tables
+PRINT 'Inserting customer data'
+INSERT INTO Customers(FirstName, LastName, [Address], City, Province, PostalCode)
+    VALUES ('Clark', 'Kent', '344 Clinton Street', 'Metropolis', 'DE', 'S0S0N0')
+INSERT INTO Customers(FirstName, LastName, [Address], City, Province, PostalCode)
+    VALUES ('Jimmy', 'Olsen', '242 River Close', 'Bakerline', 'DE', 'B4K3R1')
