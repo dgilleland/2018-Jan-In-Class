17efd31d628d27383ef2c93130f26155a526cad9
Dan Gilleland <dgilleland@nait.ca>
2018-02-27 10:11:42 -0700

    Starter file on B - Simple Select Exercise

---
+++ D-SQL/B - Simple Select Exercise.sql
@@ -0,0 +1,39 @@
+--Simple Select Exercise 2
+-- This set of exercises demonstrates performing simple Aggregate functions
+-- to get results such as SUM(), AVG(), COUNT() 
+-- All aggregates are done using built-in functions in the database
+Use IQSCHOOL
+GO
+
+--1.	Select the average Mark from all the Marks in the registration table
+
+
+--1.a.  Show the average mark, the total of all marks, and a count of all marks.
+
+
+--2.	Select the average Mark of all the students who are taking DMIT104
+
+
+--3.	Select how many students are there in the Student Table
+
+
+--4.	Select how many students are taking (have a grade for) DMIT152
+
+
+--5.	Select the average payment amount for payment type 5
+
+
+-- Given that there are some other aggregate methods like MAX(columnName) and MIN(columnName), complete the following two questions:
+--6. Select the highest payment amount
+
+
+--7.	 Select the lowest payment amount
+
+
+--8. Select the total of all the payments that have been made
+
+
+--9. How many different payment types does the school accept?
+
+--10. How many students are in club 'CSS'?
+
