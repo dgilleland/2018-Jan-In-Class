6f7d915494350a1dc0ac037209e8a72081615b73
Dan Gilleland <dgilleland@nait.ca>
2018-02-27 11:34:40 -0700

    Fix USE Statement

--- D-SQL/C - Simple Select Exercise.sql
+++ D-SQL/C - Simple Select Exercise.sql
@@ -2,7 +2,7 @@
 -- This sample set illustrates the GROUP BY syntax and the use of Aggregate functions
 -- with GROUP BY.
 -- It also demonstrates the HAVING clause to filter on aggregate values.
-USE IQSchool
+USE [A01-School]
 GO
 
 
