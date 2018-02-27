b9f2da66fd9296a05dd32592282712f84e7cfd7f
Dan Gilleland <dgilleland@nait.ca>
2018-02-27 10:14:46 -0700

    Starter file on C - Simple Select Exercise

---
+++ D-SQL/C - Simple Select Exercise.sql
@@ -0,0 +1,31 @@
+--Simple Select Exercise 3
+-- This sample set illustrates the GROUP BY syntax and the use of Aggregate functions
+-- with GROUP BY.
+-- It also demonstrates the HAVING clause to filter on aggregate values.
+USE IQSchool
+GO
+
+
+--1. Select the average mark for each course. Display the CourseID and the average mark
+
+
+--2. How many payments where made for each payment type. Display the Payment Type ID and the count
+
+
+--3. Select the average Mark for each studentID. Display the StudentId and their average mark
+-- TODO: Student Answer Here....
+
+
+--4. Select the same data as question 3 but only show the studentID's and averages that are > 80
+
+--5. How many students are from each city? Display the City and the count.
+
+--6. Which cities have 2 or more students from them? (HINT, remember that fields that we use in the where or having do not need to be selected.....)
+
+--7. What is the highest, lowest and average payment amount for each payment type? 
+
+--8. How many students are there in each club? Show the clubID and the count
+-- TODO: Student Answer Here....
+
+--9. Which clubs have 3 or more students in them?
+-- TODO: Student Answer Here....
