b680de819152b1b95e6734cfd3eb450aec0a3f87
Dan Gilleland <dgilleland@nait.ca>
2018-03-01 11:05:21 -0700

    Group By, Having and Order By

--- D-SQL/C - Simple Select Exercise.sql
+++ D-SQL/C - Simple Select Exercise.sql
@@ -7,22 +7,65 @@ GO
 
 
 --1. Select the average mark for each course. Display the CourseID and the average mark
+--   Let's begin by exploring the Registration table to see the data we are working with.
+SELECT  CourseId, Mark
+FROM    Registration
+ORDER BY CourseId
+--  Answer to #1
+SELECT  CourseId,                   -- This column is a non-aggregate
+        AVG(Mark) AS 'Average Mark' -- This column performs Aggregate (produce 1 value)
+FROM    Registration
+GROUP BY CourseId                   -- Group by the non-aggregate columns
+-- When performing an Aggregate function in the SELECT clause, if you have any other non-aggregate columns in the SELECT clause, then these must be listed in the GROUP BY clause.
 
-
---2. How many payments where made for each payment type. Display the Payment Type ID and the count
-
+--2. How many payments were made for each payment type. Display the Payment Type ID and the count
+SELECT  PaymentTypeID,                              -- Non-aggregate column (btw, it's a FK)
+        COUNT(PaymentTypeID) AS 'Count of Pay Type' -- Aggregate column
+FROM    Payment
+GROUP BY PaymentTypeID
+--2a. Do the same as above, but sort it from most frequent payment type to the least frequent
+SELECT  PaymentTypeID,                              -- Non-aggregate column (btw, it's a FK)
+        COUNT(PaymentTypeID) AS 'Count of Pay Type' -- Aggregate column
+FROM    Payment
+GROUP BY PaymentTypeID
+ORDER BY COUNT(PaymentTypeID)
 
 --3. Select the average Mark for each studentID. Display the StudentId and their average mark
 -- TODO: Student Answer Here....
-
+SELECT StudentID,
+       AVG(Mark) AS 'Avg Mark'
+FROM   Registration
+GROUP BY StudentID
 
 --4. Select the same data as question 3 but only show the studentID's and averages that are > 80
+SELECT StudentID,
+       AVG(Mark) AS 'Avg Mark'
+FROM   Registration
+GROUP BY StudentID
+-- The HAVING clause is where we do filtering of Aggregate information
+HAVING AVG(Mark) > 80
 
 --5. How many students are from each city? Display the City and the count.
+SELECT  City,
+        COUNT(StudentID) AS 'Student Count'
+FROM    Student
+GROUP BY City
 
 --6. Which cities have 2 or more students from them? (HINT, remember that fields that we use in the where or having do not need to be selected.....)
+SELECT  City
+--        , COUNT(StudentID) AS 'Student Count'
+FROM    Student
+GROUP BY City
+HAVING COUNT(StudentID) >= 2
 
 --7. What is the highest, lowest and average payment amount for each payment type? 
+SELECT MAX(Amount) AS 'Highest',
+       MIN(Amount) AS 'Lowest',
+       AVG(Amount) AS 'Average'
+--       , PaymentTypeID
+FROM   Payment
+GROUP BY PaymentTypeID
+
 
 --8. How many students are there in each club? Show the clubID and the count
 -- TODO: Student Answer Here....
