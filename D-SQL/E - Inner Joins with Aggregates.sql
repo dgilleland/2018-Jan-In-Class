2e45cec79b2a16f0450f39828544b23543c463c4
Dan Gilleland <dgilleland@nait.ca>
2018-03-01 11:05:51 -0700

    SQL Join Starter Templates

---
+++ D-SQL/E - Inner Joins with Aggregates.sql
@@ -0,0 +1,27 @@
+--Inner Joins With Aggregates Exercises
+USE [A01-School]
+GO
+
+--1. How many staff are there in each position? Select the number and Position Description
+
+ 
+--2. Select the average mark for each course. Display the CourseName and the average mark. Sort the results by average in descending order.
+
+
+--3. How many payments where made for each payment type. Display the PaymentTypeDescription and the count
+ -- TODO: Student Answer Here... 
+
+ 
+--4. Select the average Mark for each student. Display the Student Name and their average mark. Use table aliases in your FROM & JOIN clause.
+                                            -- I am using the same expression in my GROUP BY
+
+--5. Select the same data as question 4 but only show the student names and averages that are > 80. (HINT: Remeber the HAVING clause?)
+ -- TODO: Student Answer Here... 
+
+ 
+--6.what is the highest, lowest and average payment amount for each payment type Description? 
+ -- TODO: Student Answer Here... 
+
+ 
+--7. Which clubs have 3 or more students in them? Display the Club Names.
+ -- TODO: Student Answer Here... 
---
+++ D-SQL/F - Outer Joins.sql
@@ -0,0 +1,19 @@
+--Outer Joins Exercise
+USE [A01-School]
+GO
+
+--1. Select All position descriptions and the staff ID's that are in those positions
+
+
+--2. Select the Position Description and the count of how many staff are in those positions. Return the count for ALL positions.
+
+
+--3. Select the average mark of ALL the students. Show the student names and averages.
+
+
+--4. Select the highest and lowest mark for each student. 
+
+
+--5. How many students are in each club? Display club name and count.
+ -- TODO: Student Answer Here...
+
