bab4b47718f871dae97e22f72fba8f25c007f3a2
Dan Gilleland <dgilleland@nait.ca>
2018-03-02 11:39:43 -0700

    updated nuget + selenium + xunit + first test

---
+++ AdHoc/Web Testing/WebApp/SelfTests/ChromeAvailabilityCheck.cs
@@ -0,0 +1,21 @@
+﻿using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Xunit;
+
+namespace WebApp.SelfTests
+{
+    public class ChromeAvailabilityCheck
+    {
+        [Fact]
+        public void CanGoogle()
+        {
+            var driver = new ChromeDriver();
+            driver.Url = "https://www.google.ca";
+            driver.Navigate();
+
+        }
+    }
+}
