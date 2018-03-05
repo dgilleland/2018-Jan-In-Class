4ba42bb6c3c4e4db14c5c05958307614b7052b81
Dan Gilleland <dgilleland@nait.ca>
2018-03-05 10:39:33 -0700

    Inner Join

--- D-SQL/D - Simple Joins Exercise.sql
+++ D-SQL/D - Simple Joins Exercise.sql
@@ -10,17 +10,37 @@ FROM    Student
         ON Student.StudentID = Registration.StudentID
 
 --1.a. Select Student full names, the course ID and the course name that the students are registered in.
+SELECT  FirstName + ' ' + LastName AS 'FullName',
+        C.CourseId,
+        CourseName
+FROM    Student S
+    INNER JOIN Registration R
+        ON S.StudentID = R.StudentID
+    INNER JOIN Course C
+        ON R.CourseId = C.CourseId
 
-
---2.	Select the Staff full names and the Course ID’s they teach
-
+--2.	Select the Staff full names and the Course ID's they teach.
+--      Order the results by the staff name then by the course Id
+SELECT  DISTINCT -- The DISTINCT keyword will remove duplate rows from the results
+        FirstName + ' ' + LastName AS 'Staff Full Name',
+        CourseId
+FROM    Staff S
+    INNER JOIN Registration R
+        ON S.StaffID = R.StaffID
+ORDER BY 'Staff Full Name', CourseId
 
 --3.	Select all the Club ID's and the Student full names that are in them
 -- TODO: Student Answer Here...
 
 
 --4.	Select the Student full name, courseID's and marks for studentID 199899200.
-
+SELECT  S.FirstName + ' ' + S.LastName AS 'Student Name',
+        R.CourseId,
+        R.Mark
+FROM    Registration R
+    INNER JOIN Student S
+            ON S.StudentID = R.StudentID
+WHERE   S.StudentID = 199899200
 
 --5.	Select the Student full name, course names and marks for studentID 199899200.
 -- TODO: Student Answer Here...
