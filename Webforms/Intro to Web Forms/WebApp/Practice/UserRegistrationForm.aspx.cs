a4238cfc0b62d056ce3276cdaf824e94812f9a3f
Dan Gilleland <dgilleland@nait.ca>
2018-03-22 15:44:59 -0600

    Accept user registrations

--- Webforms/Intro to Web Forms/WebApp/Practice/UserRegistrationForm.aspx.cs
+++ Webforms/Intro to Web Forms/WebApp/Practice/UserRegistrationForm.aspx.cs
@@ -19,7 +19,14 @@ namespace WebApp.Practice
 
         protected void Page_Load(object sender, EventArgs e)
         {
+            if (!IsPostBack)
+                PopulateGridView();
+        }
 
+        private void PopulateGridView()
+        {
+            RegisteredUsersGridView.DataSource = TheUsers;
+            RegisteredUsersGridView.DataBind();
         }
 
         protected void SubmitRegistration_Click(object sender, EventArgs e)
@@ -28,11 +35,27 @@ namespace WebApp.Practice
             {
                 if(AgreeToTerms.Checked)
                 {
+                    // Make a new RegisteredUser object based off of info in
+                    // the form
+                    var newPerson = new RegisteredUser()
+                    {   // This is an "Initialization List"
+                        FirstName = FirstName.Text,
+                        LastName = LastName.Text,
+                        Email = Email.Text,
+                        Password = Password.Text,
+                        UserName = UserName.Text,
+                        AgreedToTerms = DateTime.Now
+                    };
 
+                    // Add them to the list (or DB in a real app)
+                    TheUsers.Add(newPerson);
+                    // Refresh the data in the GridView
+                    PopulateGridView();
                 }
                 else
                 {
                     // Display a message through the MessageLabel control
+                    MessageLabel.Text = "You must agree to the terms before you can be registered.";
                 }
             }
         }
